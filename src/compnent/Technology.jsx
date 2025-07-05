b7d7fc57fdef1d4dd513097a79684bd92c7a5da6
vandanafuloria <vandanafuloria02@gmail.com>
2025-07-05 23:35:41 +0530

    added border colors

--- src/compnent/Technology.jsx
+++ src/compnent/Technology.jsx
@@ -1,68 +1,68 @@
 export default function Technology() {
   return (
     <>
-      <section className="Technology-section border-2 rounded-2xl m-2 p-2">
-        <h1 className="font-bold text-4xl">Technology i am familiar with</h1>
-        <div className="tech flex justify-start flex-wrap p-4">
-          <span>
-            <i className="devicon-html5-plain colored"></i>
+      <section className="Technology-section border border-gray-700 rounded-2xl m-2 p-2">
+        <h1 className="font-bold text-xl text-pink-600 p-4">
+          Technologies i am familiar with
+        </h1>
+        <div className="tech flex justify-start flex-wrap p-4 w-9/10 m-auto gap-3">
+          <span className="w-20 text-5xl border-red-500 border hover:shadow-lg shadow-pink-800">
+            <i className="devicon-html5-plain colored font-full"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl border border-blue-500">
             <i class="devicon-css3-plain colored"></i>
           </span>
-          <span>
-            <i class="devicon-css3-plain colored"></i>
-          </span>
-          <span>
+
+          <span className="w-20 text-5xl border border-yellow-400">
             <i class="devicon-javascript-plain colored"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl border border-blue-500">
             <i class="devicon-react-original colored"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl border border-red-500">
             <i class="devicon-git-plain colored"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl text-white border border-gray-400">
             <i class="devicon-github-original"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl border-blue-800 border">
             <i class="devicon-tailwindcss-original colored"></i>
           </span>
-          <span>
+          <span className="w-20 border-purple-400 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
           </span>
-          <span>
+          <span className="w-20 text-5xl border-purple-400 border">
             <i class="devicon-redux-original colored"></i>
           </span>
-          <span>
+          <span className="w-20 text-5xl border-blue-400 border">
             <i class="devicon-typescript-plain colored"></i>
           </span>
-          <span>
+          <span className="w-20 border-pink-400 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" />
           </span>
-          <span>
+          <span className="w-20 border-blue-400 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg" />
           </span>
-          <span>
+          <span className="w-20 border-green-500 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original.svg" />
           </span>
 
-          <span>
-            <i class="devicon-vercel-original colored"></i>
+          <span className="w-20 text-5xl text-white border-gray-700 border">
+            <i class="devicon-vercel-original"></i>
           </span>
-          <span>
+          <span className="w-20 border-yellow-500 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" />
           </span>
-          <span>
+          <span className="w-20 border-blue-800 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/windows11/windows11-original.svg" />
           </span>
-          <span>
+          <span className="w-20 border-blue-400 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" />
           </span>
-          <span>
+          <span className="w-20 border-purple-400 border">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg" />
           </span>
-          <span>
+          <span className="w-20 text-5xl border-red-400 border">
             <i class="devicon-postman-plain colored"></i>
           </span>
         </div>
